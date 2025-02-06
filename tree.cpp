bb15518cb4550f26b76c1ff56dcb5789e8023e37
Ghanshyam Gupta <147191967+ghanshyamgcs22@users.noreply.github.com>
2025-02-06 18:32:09 +0530

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -5,12 +5,12 @@ Author ghanshyamgcs22
 */
 #include <bits/stdc++.h>
 using namespace std;
-
+using ll=long long;
 //<-------------------------- Re-rooting ------------------------>
-// int in[200005];
-// int out[200005];
+// ll in[200005];
+// ll out[200005];
 
-// void dfs(int node, int par, vector<int> adj[]) {
+// void dfs(int node, int par, vector<ll> adj[]) {
 //     in[node] = 0;
     
 //     for (auto &it : adj[node]) {
@@ -20,8 +20,8 @@ using namespace std;
 //     }
 // }
 
-// void dfs2(int node, int par, vector<int> adj[]) {
-//     int mx1 = -1, mx2 = -1, child = -1;
+// void dfs2(int node, int par, vector<ll> adj[]) {
+//     ll mx1 = -1, mx2 = -1, child = -1;
     
 //     for (auto &it : adj[node]) {
 //         if (it != par) {
@@ -59,11 +59,11 @@ using namespace std;
 //<-------------------------- Re-rooting End------------------------>
 
 
-vector<int> eulr_tour_type1_v;
-unordered_map<int, int> tour_type1_mp1;
-int tour_type1_idx = 0;
+vector<ll> eulr_tour_type1_v;
+unordered_map<ll, ll> tour_type1_mp1;
+ll tour_type1_idx = 0;
 
-void dfs_eulr_tour_type1(int node, int par, vector<int> adj[]) {
+void dfs_eulr_tour_type1(int node, int par, vector<ll> adj[]) {
     eulr_tour_type1_v.push_back(node);
     tour_type1_mp1[tour_type1_idx] = node;
     tour_type1_idx++;
@@ -79,11 +79,11 @@ void dfs_eulr_tour_type1(int node, int par, vector<int> adj[]) {
 }
 
 
-vector<int> eulr_tour_type2_v;
-unordered_map<int, pair<int, int>> tour_type2_mp;
+vector<ll> eulr_tour_type2_v;
+unordered_map<ll, pair<ll, ll>> tour_type2_mp;
 int tour_type2_idx = 0;
 
-void dfs_eulr_tour_type2(int node, int par, vector<int> adj[]) {
+void dfs_eulr_tour_type2(int node, int par, vector<ll> adj[]) {
     eulr_tour_type2_v.push_back(node);
     tour_type2_mp[tour_type2_idx].first = node;
     int curr_idx = tour_type2_idx;  
@@ -100,14 +100,14 @@ void dfs_eulr_tour_type2(int node, int par, vector<int> adj[]) {
 }
 
 
- int dp_tsp[13][1 << 13];
+ ll dp_tsp[13][1 << 13];
 
-    int tsp(int i, int mask, vector<vector<int>>& cost) {
+    ll tsp(int i, int mask, vector<vector<ll>>& cost) {
         if (mask == 0) return 0; // All nodes visited
 
         if (dp_tsp[i][mask] != -1) return dp_tsp[i][mask];
 
-        int ans = INT_MAX;
+        ll ans = INT_MAX;
         for (int j = 0; j < cost.size(); j++) {
             if (mask & (1 << j)) { // If node `j` is not visited
                 ans = min(ans, cost[i][j] + tsp(j, mask ^ (1 << j), cost));
@@ -116,7 +116,7 @@ void dfs_eulr_tour_type2(int node, int par, vector<int> adj[]) {
 
         return dp_tsp[i][mask] = ans;
     }
-void dp_on_tree(int node,vector<int>adj[],int par,vector<int>&dp)
+void dp_on_tree(int node,vector<ll>adj[],int par,vector<ll>&dp)
 {
 
     dp[node]=1;
@@ -129,7 +129,7 @@ void dp_on_tree(int node,vector<int>adj[],int par,vector<int>&dp)
     }
 }
  
-void binary_lifting(int node,int par,vector<int>adj, vector<vecor<int>>&dp)
+void binary_lifting(int node,int par,vector<ll>adj, vector<vecor<ll>>&dp)
 {
      //  base case
      dp[node][0]=par;
@@ -150,19 +150,19 @@ void solve_tree()
 {
    int n;
 	cin>>n;
-	vector<int>adj[n+1];
+	vector<ll>adj[n+1];
 	
 	// vector<int>dp(n+1);
 	for(int i=0;i<n-1;i++)
 	{ 
-        int u,v;cin>>u>>v;
+        ll u,v;cin>>u>>v;
 	    adj[u].push_back(v);
 	    adj[v].push_back(u);
 	}
 
     int q;
     cin>>q;
-    vector<vecor<int>>dp(n+1,vector<int>(17,0));
+    vector<vecor<ll>>dp(n+1,vector<int>(17,0));
     binary_lifting(1,0,adj,dp);  
     while(q--)
     {
