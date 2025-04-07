e8d50f59d8e4ed9483738e51f6998ead3e7bfe42
Ghanshyam Gupta <147191967+ghanshyamgcs22@users.noreply.github.com>
2025-04-08 03:40:55 +0530

    Update cp_temp.cpp

--- cp_temp.cpp
+++ cp_temp.cpp
@@ -32,7 +32,23 @@ const int BASE1 = 31;
 const int BASE2 = 53;
 vector<ll> prefixHash1, prefixHash2;
 vector<ll> power1, power2;
- 
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = 
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+
 struct pair_hash
 {
     template <typename T1, typename T2>
